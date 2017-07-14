64d706c5faa66137a3016f23681d09213f6ea106
baiyangcao <baiyangcao@msn.cn>
2017-07-14 10:17:56 +0800

    添加样例代码测试 TuringBot 和 Voicer 类

--- src/main/kotlin/demo/Demo.kt
+++ src/main/kotlin/demo/Demo.kt
@@ -1,9 +1,23 @@
 package demo
 
+import ifly.Voicer
+import kotlinx.coroutines.experimental.*
+import turing.TuringBot
+
 /**
  * Created by Administrator on 2017/6/11.
  */
 
-fun main(args:Array<String>){
-    println("msc.example")
+fun main(args:Array<String>) = runBlocking<Unit> {
+    var voicer = Voicer()
+    var bot = TuringBot()
+
+    voicer.speak("嗨，我是小图！")
+
+    var result = ""
+    while (isActive && !result.contains("再见")) {
+        voicer.listening { words -> result = words }
+        var response = bot.talk(result)
+        voicer.speak(response)
+    }
 }
