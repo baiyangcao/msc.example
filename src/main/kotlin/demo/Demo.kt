751c68cbd46275a289ac910f0d3a2b6a522ca72e
baiyangcao <baiyangcao@msn.cn>
2017-07-15 10:30:08 +0800

    添加处理过程结果显示，用于测试调试

--- src/main/kotlin/demo/Demo.kt
+++ src/main/kotlin/demo/Demo.kt
@@ -12,12 +12,14 @@ fun main(args:Array<String>) = runBlocking<Unit> {
     var voicer = Voicer()
     var bot = TuringBot()
 
-    voicer.speak("嗨，我是小图！")
+    voicer.speak("你好，我是小图！")
 
     var result = ""
     while (isActive && !result.contains("再见")) {
         voicer.listening { words -> result = words }
+        println(result)
         var response = bot.talk(result)
+        println(response)
         voicer.speak(response)
     }
 }
